e36c460c0aa41d0ef2e50acb0bb71a37ee7d0bc2
Nick Chermak <nac136@pitt.edu>
2024-01-24 15:34:08 -0500

    Project 1 - no bonus

--- projects/project1/bitset_test.cpp
+++ projects/project1/bitset_test.cpp
@@ -74,3 +74,53 @@ TEST_CASE( "Test combined", "[bitset]" ) {
         REQUIRE(((b.test(i + (1<<11)) == true && s.at(i + (1<<11)) == '0') || (b.test(i + (1<<11)) == false && s.at(i + (1<<11)) == '1')));
     }
 }
+
+
+//added test cases
+
+TEST_CASE( "Test set with size init part 2", "[bitset]" ) {
+    std::string s("0001000001");
+    Bitset b(10);
+    b.set(3);
+    b.set(9);
+    REQUIRE(b.size() == 10);
+    REQUIRE(b.good());
+    REQUIRE(b.asString().compare(s) == 0);
+}
+
+TEST_CASE( "Test toggle and set", "[bitset]"){
+    std::string s("0000000000");
+    Bitset b(10);
+    b.set(3);
+    b.set(9);
+    b.toggle(3);
+    b.toggle(9);
+    REQUIRE(b.size() == 10);
+    REQUIRE(b.good());
+    REQUIRE(b.asString().compare(s) == 0);
+}
+
+TEST_CASE( " Test different construction of string", "[bitset]"){
+    std::string s("001101110100110011011010010010101010101010111110100101010101101011011011101000010100100");
+    Bitset b(s);
+    REQUIRE(b.size() == s.size());
+    REQUIRE(b.good());
+    REQUIRE(b.asString().compare(s) == 0);
+}
+
+TEST_CASE( "Invalid string", "[bitset]" ) {
+    std::string s("01010010111010100111010110.0");
+    Bitset b(s);
+    REQUIRE_FALSE(b.good());
+}
+
+TEST_CASE("toggle through","[bitset]"){
+    std::string s("1111111111");
+    Bitset b(10);
+    for (int i = 0; i < 10; i++){
+        b.toggle(i);
+    }
+    REQUIRE(b.size() == 10);
+    REQUIRE(b.good());
+    REQUIRE(b.asString().compare(s) == 0);
+}

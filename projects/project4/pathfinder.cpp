6270660af6aede744bca02322eb11e747434a86b
Nick Chermak <nac136@pitt.edu>
2024-04-02 19:20:06 -0400

    pathfinder maze solver

--- projects/project4/pathfinder.cpp
+++ projects/project4/pathfinder.cpp
@@ -3,6 +3,11 @@
 #include "lib/image.h"
 #include "list.hpp"
 #include "queue.hpp"
+#include <vector>
+
+struct State {int r; int c;};
+
+int breadthFirst(Image<Pixel>);
 
 int main(int argc, char *argv[])
 {
@@ -21,8 +26,75 @@ int main(int argc, char *argv[])
   Image<Pixel> image = readFromFile(input_file);
 
   // TODO
+  breadthFirst(image);
 
   // Write solution image to file
   writeToFile(image, output_file);
   
+  
 }
+
+int breadthFirst(Image<Pixel> image)
+{
+  Queue<State, List<State>> frontier;
+  std::vector<std::vector<bool>> explored(image.width(),std::vector<bool>(image.height(),0));
+  State s;
+
+  //set red pixel as start
+  for (int i = 0; i < image.width(); i++)
+  {
+    for (int j = 0; j < image.height(); j++)
+    {
+      if(image(i,j) == RED)
+        s = {i,j};
+    }
+  }
+  
+  frontier.enqueue(s);
+  while(!frontier.isEmpty()){
+    s = frontier.peekFront();
+    
+    frontier.dequeue();
+
+    //are we at the end?
+    if(s.r == 0 || s.r == image.width()-1 || s.c == 0 || s.c == image.height()-1 )
+    {
+      image(s.r,s.c) = GREEN;
+      return EXIT_SUCCESS;
+    }
+
+    if(image(s.r+1,s.c) == WHITE && explored[s.r+1][s.c] != 1){
+      State f = {s.r+1,s.c};
+      frontier.enqueue(f);
+      explored[s.r+1][s.c] == 1;
+    }
+    if(image(s.r-1,s.c) == WHITE && explored[s.r-1][s.c] != 1){
+      State f = {s.r-1,s.c};
+      frontier.enqueue(f);
+      explored[s.r-1][s.c] == 1;
+    }
+    if(image(s.r,s.c+1) == WHITE && explored[s.r][s.c+1] != 1){
+      State f = {s.r,s.c+1};
+      frontier.enqueue(f);
+      explored[s.r][s.c+1] == 1;
+    }
+    if(image(s.r,s.c-1) == WHITE && explored[s.r][s.c-1] != 1){
+      State f = {s.r,s.c-1};
+      frontier.enqueue(f);
+      explored[s.r][s.c-1] == 1;
+    }
+  }
+  return EXIT_FAILURE;
+}
+
+
+//2 data structures
+
+// empty queue is failure
+
+//create q
+//set red point to poiunt
+//check to see if at end
+//check left, right, up, down
+
+//have matrix that can set and check??
--- projects/project4/queue.tpp
+++ projects/project4/queue.tpp
@@ -4,25 +4,32 @@ template <typename T, typename L>
 void Queue<T,L>::enqueue(const T& item)
 {
   // TODO
+  internal_list.insert(internal_list.getLength(), item);
 }
 
 template <typename T, typename L>
 void Queue<T,L>::dequeue()
 {
   // TODO
+  if(internal_list.getLength() == 0){
+    throw(std::range_error("Cannot Dequeue, list is size 0"));
+  }
+  else{
+    internal_list.remove(0);
+  }
 }
 
 template <typename T, typename L>
 T Queue<T,L>::peekFront()
 {
   // TODO
-  return T();
+  return internal_list.getEntry(0);
 }
 
 template <typename T, typename L>
 bool Queue<T,L>::isEmpty() const
 {
   // TODO
-  return false;
+  return internal_list.isEmpty();
 }
 
